bf8a8a475a2e3bf8cac2336a3ad1152042d65ab5
FernandaDiaz12 <53801703+FernandaDiaz12@users.noreply.github.com>
2019-10-30 11:34:38 -0600

    Update 4. AlignmentQC.sh

--- bin/4. AlignmentQC.sh
+++ bin/4. AlignmentQC.sh
@@ -1,11 +1,15 @@
+### Post-alignment QC
+#Once you have installed RSeQC
+
+## I performed the clipping_profile parameter
 
 ## coverage analysis
 ##clipping_profile.py -h
 
-sam_dirs=~/Documents/FernandaRNA/SAM_STAR_files/*.sam
+sam_dirs= ~data/sam_files
 
 for file in $sam_dirs; do
        samp_name=$(echo $file | sed "s/.*\///g")
-       echo RUNNING:clipping_profile.py -i ~/Documents/FernandaRNA/SAM_STAR_files/$samp_name -o data/clip/$samp_name
+       echo RUNNING:clipping_profile.py -i ~data/sam_files$samp_name -o data/clip/$samp_name
        clipping_profile.py -i ~/Documents/FernandaRNA/SAM_STAR_files/$samp_name -s "PE"  -o data/clip/$samp_name
 done

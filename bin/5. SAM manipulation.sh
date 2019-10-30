6ed42b52b635a6dc7b6afcac13b68c957675e444
FernandaDiaz12 <53801703+FernandaDiaz12@users.noreply.github.com>
2019-10-30 11:49:44 -0600

    Update 5. SAM manipulation.sh

--- bin/5. SAM manipulation.sh
+++ bin/5. SAM manipulation.sh
@@ -1,17 +1,25 @@
 ##SAM manipulation
 ## samtools
+###When you have your SAM files post-alignment
 
+####The loop will perform the three following steps:
+# 1. Transform SAM > BAM
+# 2. Sort by coordinates
+# 3. Index BAM —> Bam.Bai
 
-#Transform SAM > BAM
+#Create output directories
+cd ..
+cd data
+mkdir BAM
+mkdir Sorted
+mkdir indexed
 
-samtools view -bS SAM_STAR_files/*Aligned.out.sam > BAM/
+cd ..
+cd bin
 
-
-#Sort by coordinates
-
-samtools sort BAM/*.bam -o Sorted/
-
-
-#Index BAM —> Bam.Bai
-
-> samtools index *.sorted.bam 
+##Loop
+for i in $(ls ../data/sam_files/*.Aligned.out.sam) do;
+samtools view -bS ../data/sam_files/${prefix} > BAM;
+samtools sort BAM/*.bam -o Sorted/;
+samtools index *.sorted.bam;
+done

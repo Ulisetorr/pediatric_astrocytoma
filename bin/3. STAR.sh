87d16d7d20eb5d9630c566088f5734f390e815e6
FernandaDiaz12 <53801703+FernandaDiaz12@users.noreply.github.com>
2019-10-30 11:25:24 -0600

    Update 3. STAR.sh

--- bin/3. STAR.sh
+++ bin/3. STAR.sh
@@ -1,18 +1,27 @@
 # Alignment with STAR 
-# Download start and copy the binary files to usr/local/bin/
-# 
 
 #To create index genome with STAR 
-# Download latest version of human genome and annotations
+# Download latest version of the human genome and annotations
+
 # http://www.ensembl.org/info/data/ftp/index.html — from ensemble download files: <Homo_sapiens.GRCh38.dna.alt.fa.gz> and <Homo_sapiens.GRCh38.96.chr_patch_hapl_scaff.gtf.gz>
 # Save into the working directory and gunzip them or unzip them
-# Create index genome:
-mkdir -p star_indices
 
-STAR --runThreadN 18 --runMode genomeGenerate --genomeDir star_indices/ --genomeFastaFiles Homo_sapiens.GRCh38.dna.primary_assembly.fa --sjdbGTFfile Homo_sapiens.GRCh38.96.gtf --sjdbOverhang 100
-STAR --runThreadN 18 --genomeDir STAR/star_indices/ --readFilesIn raw_data/A10A_S16_L004_R1_001.fastq raw_data/A10A_S16_L004_R2_001.fastq --outFileNamePrefix SAM_STAR_files02/A10A_
+###These files can also be found in the data directory but is better if you download them every time you'll run this script to assure updated fasta files
+
+# Create index genome:
+mkdir -p data/star_indices
+
+STAR --runThreadN 18 --runMode genomeGenerate --genomeDir ../data/star_indices --genomeFastaFiles ../data/Homo_sapiens.GRCh38.dna.primary_assembly.fa --sjdbGTFfile ../data/Homo_sapiens.GRCh38.96.gtf --sjdbOverhang 100
 
 
 ##Alignment
 
-STAR --runThreadN 18 --genomeDir STAR/star_indices/ --readFilesIn raw_data/A10A_S16_L004_R1_001.fastq raw_data/A10A_S16_L004_R2_001.fastq 
+cd ..
+cd data
+cd Fastq
+
+##Loop for alignment
+for prefix in $(ls *.fastq | rev | cut -c 14- | rev | uniq);
+do;
+STAR --readFilesIn ${prefix}_R1_002.fastq ${prefix}_R2_002.fastq; --runThreadN 18 --runMode alignReads --genomeDir ../star_indices --outFileNamePrefix ${prefix%.fastq}_; 
+done 

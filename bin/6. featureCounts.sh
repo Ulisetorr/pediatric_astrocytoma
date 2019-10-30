d341fbc9495ba9ffa11f431d8a58d6ae7df1459b
FernandaDiaz12 <53801703+FernandaDiaz12@users.noreply.github.com>
2019-10-30 12:48:23 -0600

    Update 6. featureCounts.sh

--- bin/6. featureCounts.sh
+++ bin/6. featureCounts.sh
@@ -1,10 +1,14 @@
 ##Create a count matrix using featureCounts from  Subread
-
-
-##For a matrix for each sample
-
-
-
-
+##Once you have installed the Subread package 
 ##For a single matrix with all the samples
-featureCounts -T 18 -s 1 -a STAR/Homo_sapiens.GRCh38.96.gtf -o Conteo/featureCounts/Astocytoma.txt ~/Documents/FernandaRNA/BAM/*.bam -B -p -G Conteo/featureCounts/Homo_sapiens.GRCh38.dna.primary_assembly.fa
+
+#Create output directory 
+cd ..
+cd data 
+mkdir Conteo 
+
+cd .. 
+cd bin 
+
+##Run featureCounts 
+featureCounts -T 18 -s 1 -a ../data/Homo_sapiens.GRCh38.96.gtf -o ../data/Conteo/Astocytoma.txt ~/data/Sorted/*.sorted.bam -B -p 

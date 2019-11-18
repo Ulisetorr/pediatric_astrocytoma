c1db6590f394dd4d5891db199c6312d71d752108
FernandaDiaz12 <53801703+FernandaDiaz12@users.noreply.github.com>
2019-11-18 15:30:44 -0600

    Add libraries installation

---
+++ bin/7. edgeR/7.0 edgeR_libraries(just once).R
@@ -0,0 +1,12 @@
+###Script to install libraries 
+##Just install once!!!! 
+###Install in the given order 
+##Run from bin directory
+install.packages("BiocManager")
+BiocManager::install("edgeR")
+BiocManager::install("limma")
+BiocManager::install("gplots")
+BiocManager::install("org.Mm.eg.db")
+BiocManager::install("RColorBrewer")
+BiocManager::install("Glimma")
+install.packages("statmod")
